23f679f1d2394ae1449ab62ca695cf3baf62922b
Administrator <Administrator@WG36BCFJ7BMTWNZ>
2018-12-06 20:52:14 +0200

    Update project files.

--- CFW2OFW Splitter/DIRSPLIT.cpp
+++ CFW2OFW Splitter/DIRSPLIT.cpp
@@ -44,8 +44,8 @@ bool DIRSPLIT::split()
 		qDebug() <<"2" <<DestPath + f.fileInfo().absolutePath().mid(path.length());
 		QDir().mkpath(DestPath + f.fileInfo().absolutePath().mid(path.length()));
 		for each (const QString &templatefile in templatefiles) {
-			qDebug() <<"3"<< DestPath + templatefile.mid(path.length(), templatefile.size() - templatefile.lastIndexOf('\\'));
-			QDir().mkpath(DestPath + templatefile.mid(path.length(), templatefile.size() - templatefile.lastIndexOf('\\')));
+			qDebug() <<"3"<< DestPath + templatefile.mid(path.length(), templatefile.size() - templatefile.lastIndexOf(QChar('\\')));
+			QDir().mkpath(DestPath + templatefile.mid(path.length(), templatefile.size() - templatefile.lastIndexOf(QChar('\\'))));
 			qDebug() <<"4" <<DestPath + templatefile.mid(path.length());
 			QFile::copy(templatefile, DestPath + templatefile.mid(path.length()));
 		}
