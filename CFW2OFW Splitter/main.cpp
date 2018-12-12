7ffa8bbd27249f0f1c24db290dafbb1a3644e848
Administrator <Administrator@WG36BCFJ7BMTWNZ>
2018-12-12 22:53:27 +0200

    Update project files.

--- CFW2OFW Splitter/main.cpp
+++ CFW2OFW Splitter/main.cpp
@@ -7,36 +7,26 @@
 #include "pkg.h"
 #include "dirsplit.h"
 
-bool splitandcreatepkg(QString path)
-{
-	DIRSPLIT ds(path, 4294705152, QStringList() << "PARAM.SFO" << "ICON0.PNG" << "USRDIR\\EBOOT.BIN");
-	if (ds.split()) {
-		for each (const QString &d in ds.entryList()) {
-			qDebug() << d << endl;
-			PKG(d).Generate_Package();
-		}
-	}
-	else {
-		PKG(path).Generate_Package();
-	}
-}
-
-
 int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
 	qDebug() << " --- CFW2OFW Splitter v1 ---" << endl << "  -- a.yassin@msn.com --" << endl;
-	if (argc > 0)
-		splitandcreatepkg(argv[1]);
-	else
+	QStringList gamedirectorys(a.arguments());
+	if (gamedirectorys.isEmpty())
+		gamedirectorys << QDir("").entryList(QStringList() << "BL???????" << "BC???????", QDir::Dirs);
+	for each (QString gamedirectory in gamedirectorys)
 	{
-		QDirIterator dir(QCoreApplication::applicationDirPath(), QStringList() << "???????", QDir::Dirs);
-		while (dir.hasNext()) {
-			dir.next();
-			splitandcreatepkg(dir.path());
+		DIRSPLIT dirsplit(gamedirectory, 4294705152, QStringList() << "PARAM.SFO" << "ICON0.PNG" << "USRDIR\\EBOOT.BIN");
+		if (dirsplit.split()) {
+			for each (const QString &entry in dirsplit.entryList()) {
+				qDebug() << entry << endl;
+				PKG(entry).Generate_Package();
+			}
+		}
+		else {
+			PKG(gamedirectory).Generate_Package();
 		}
 	}
-
 	qDebug() << "Press any key to continue . . ."; getchar();
 	return FALSE;
 }
