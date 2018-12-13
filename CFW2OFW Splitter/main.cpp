d51d40d8d2a62318d6eeba06279f5d8788a9bc45
Administrator <Administrator@AYASSIN-OUK981F>
2018-12-14 01:27:54 +0200

    Update project files.

--- CFW2OFW Splitter/main.cpp
+++ CFW2OFW Splitter/main.cpp
@@ -1,6 +1,6 @@
 #include <Windows.h>
 #include <QtCore/QCoreApplication>
-#include <Qtcore/QDirIterator>
+#include <Qtcore/QDir>
 #include <Qtcore/QString>
 #include <Qtcore/qDebug>
 #include <QtCore/qstringlist.h>
@@ -10,10 +10,10 @@
 int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
-	qDebug() << " --- CFW2OFW Splitter v1 ---" << endl << "  -- a.yassin@msn.com --" << endl;
+	qDebug() << " --- CFW2OFW Splitter v2 ---" << endl << "  -- a.yassin@msn.com --" << endl;
 	QStringList gamedirectorys(a.arguments());
 	if (gamedirectorys.isEmpty())
-		gamedirectorys << QDir("").entryList(QStringList() << "BL???????" << "BC???????", QDir::Dirs);
+		gamedirectorys << QDir("").entryList(QStringList() << "BL???????" << "BC???????" << "NP???????", QDir::Dirs);
 	for each (QString gamedirectory in gamedirectorys)
 	{
 		DIRSPLIT dirsplit(gamedirectory, 4294705152, QStringList() << "PARAM.SFO" << "ICON0.PNG" << "USRDIR\\EBOOT.BIN");
