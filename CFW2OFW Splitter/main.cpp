c87574e6250e38af503e1170825a511efdfc11c2
Administrator <Administrator@AYASSIN-OUK981F>
2018-12-15 01:16:42 +0200

    Update project files.

--- CFW2OFW Splitter/main.cpp
+++ CFW2OFW Splitter/main.cpp
@@ -12,13 +12,15 @@ int main(int argc, char *argv[])
 	QCoreApplication a(argc, argv);
 	qDebug() << " --- CFW2OFW Splitter v2 ---" << endl << "  -- a.yassin@msn.com --" << endl;
 	QStringList gamedirectorys(a.arguments());
+	if(gamedirectorys.at(0) == QDir::toNativeSeparators(a.applicationFilePath()))
+		gamedirectorys.removeAt(0);
 	if (gamedirectorys.isEmpty())
 		gamedirectorys << QDir(QDir::currentPath()).entryList(QStringList() << "BL??????" << "BC??????" << "NP??????", QDir::Dirs | QDir::NoDotAndDotDot);
 	for each (QString gamedirectory in gamedirectorys)
 	{
-		//if (!gamedirectory.contains(QDir::currentPath()))
+		if (!gamedirectory.contains(QDir::toNativeSeparators(QDir::currentPath())))
 			gamedirectory = QDir::currentPath() + QDir::separator() + gamedirectory;
-		qDebug() << gamedirectory;
+		qDebug() << gamedirectory << endl;
 		DIRSPLIT dirsplit(gamedirectory, 4294705152, QStringList() << "PARAM.SFO" << "ICON0.PNG" << "USRDIR\\EBOOT.BIN");
 		if (dirsplit.split()) {
 			for each (const QString &entry in dirsplit.entryList()) {
