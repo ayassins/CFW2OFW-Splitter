14dd8b4823624748ab87eb453efb034f33e29e3f
Administrator <Administrator@AYASSIN-OUK981F>
2019-02-01 05:18:21 +0200

    Update project files.

--- CFW2OFW Splitter/main.cpp
+++ CFW2OFW Splitter/main.cpp
@@ -10,7 +10,7 @@
 int main(int argc, char *argv[])
 {
 	QCoreApplication a(argc, argv);
-	qDebug() << " --- CFW2OFW Splitter v2 ---" << endl << "  -- a.yassin@msn.com --" << endl;
+	qDebug() << " --- CFW2OFW Splitter v3 ---" << endl << "  -- a.yassin@msn.com --" << endl;
 	QStringList gamedirectorys(a.arguments()); gamedirectorys.removeFirst();
 	if (argc < 2)
 		gamedirectorys << QDir(QDir::currentPath()).entryList(QStringList() << "BL??????" << "BC??????" << "NP??????", QDir::Dirs | QDir::NoDotAndDotDot);
