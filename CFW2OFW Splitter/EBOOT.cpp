0c787071312aeaa9372720ad3a453581be313cf3
a.yassin <a.yassin@msn.com>
2019-04-10 20:36:51 +0200

    Update project files.

--- CFW2OFW Splitter/EBOOT.cpp
+++ CFW2OFW Splitter/EBOOT.cpp
@@ -11,36 +11,31 @@ EBOOT::~EBOOT() {
 }
 
 
-bool EBOOT::ismagic() {
-	return ((magic == 0x53434500));
-}
-
-
-bool EBOOT::isversion() {
-	return ((version == 0x02000000));
-}
-
-
-bool EBOOT::open(QFile::OpenMode flags) {
-	if (!f.open(flags))
+bool EBOOT::open() {
+	if (!f.open(QFile::ReadOnly))
 		return false;
 	QDataStream in(&f);
 	in.setByteOrder(QDataStream::BigEndian);
+	quint32 magic, version;
 	in >> magic >> version;
-	if (!ismagic())
+	if (magic != 0x53434500)
 		return false;
-	if (!isversion())
+	if (version != 0x02000000)
 		return false;
+}
+
+
+bool EBOOT::close() {
+	f.close();
 }
 
 
 QString EBOOT::Content_ID() {
-	//if (!iseboot())
-		//return QString();
 	QDataStream in(&f);
 	in.device()->seek(0x450);
 	QByteArray ContentID(0x24, '\0');
-	in.readRawData(ContentID.data(), 0x24);
+	if (in.readRawData(ContentID.data(), 0x24) != 0x24)
+		return false;
 	ContentID.replace("PATCH", "GAME0");
 	ContentID.replace("SHIP0", "GAME0");
 	return ContentID;
--- CFW2OFW Splitter/EBOOT.h
+++ CFW2OFW Splitter/EBOOT.h
@@ -7,12 +7,10 @@ class EBOOT
 public:
 	EBOOT(const QString & path);
 	~EBOOT();
-	bool open(QFile::OpenMode flags);
+	bool open();
+	bool close();
 	QString Content_ID();
 private:
 	QFile f;
 	QString path;
-	quint32 magic, version;
-	bool ismagic();
-	bool isversion();
 };
