ecb5819ec26be5a5679b1c15cd6bf15b831f78fe
Administrator <Administrator@AYASSIN-OUK981F>
2019-04-12 18:01:03 +0200

    Update project files.

--- CFW2OFW Splitter/PKG.cpp
+++ CFW2OFW Splitter/PKG.cpp
@@ -22,36 +22,42 @@ bool PKG::generate_debug_package() {
 	PARAM p(path + "\\PARAM.SFO");
 	if (!p.isparam())
 		return false;
-
+	QString Version = p.at(PARAM::VERSION);
+	QString App_Ver = p.at(PARAM::APP_VER);
+	QString Category = p.at(PARAM::CATEGORY);
+	if(!p.insert(PARAM::TARGET_APP_VER, "00.01"))
+		return false;
+	if (Category == "DG" || Category == "HG" || Category == "HD" || Category == "AT")
+		if (!p.insert(PARAM::APP_VER, "01.00"))
+			return false;
+	if (!p.close())
+		return false;
 	EBOOT e(path + "\\USRDIR\\EBOOT.BIN");
 	//if (!e.iseboot())
 		//return false;
-	qDebug() << "ok";
+	QString Content_ID = e.Content_ID();
+	if (!e.close())
+		return false;
 	QFile f(package_conf);
 	if (!f.open(QIODevice::WriteOnly))
 		return false;
-	QString Content_ID = e.Content_ID();
 	QString package_part_number;
 	QString Title_ID = path.mid(1 + path.lastIndexOf(QChar('\\')));
 	if (Title_ID.contains(QChar('_'))) {
 		package_part_number = ".part" + Title_ID.right(1);
 		Title_ID.remove(Title_ID.indexOf(QChar('_')), 2);
 	}
-	QString Version = p.at(PARAM::VERSION);
 	QTextStream out(&f);
 	out << "ContentID = " << Content_ID << endl
 		<< "Klicensee = 0x00000000000000000000000000000000" << endl
 		<< "DRMType = Free" << endl
 		<< "InstallDirectory = " << Title_ID << endl
 		<< "PackageVersion = " << Version << endl;
-	QString Category = p.at(PARAM::CATEGORY);
-	PARAM(path + "\\PARAM.SFO").insert(PARAM::TARGET_APP_VER, "00.01");
 	if (Category == "GD") {
 		out << "ContentType = GameData" << endl
 			<< "PackageType = DiscGamePatch" << endl;
 	}
 	else if (Category == "DG" || Category == "HG" || Category == "HD" || Category == "AT") {
-		PARAM(path + "\\PARAM.SFO").insert(PARAM::APP_VER, "01.00");
 		out << "ContentType = GameExec" << endl
 			<< "PackageType = HDDGamePatch" << endl;
 	}
@@ -59,7 +65,6 @@ bool PKG::generate_debug_package() {
 		return false;
 	f.close();
 	QProcess proc;
-	QString App_Ver = p.at(PARAM::APP_VER);
 	proc.setProcessChannelMode(QProcess::ForwardedChannels);
 	proc.start(psn_package_npdrm, QStringList() << "-n" << "-f" << package_conf << path);
 	if (!proc.waitForStarted())
@@ -68,8 +73,10 @@ bool PKG::generate_debug_package() {
 		return false;
 	if (proc.exitCode() != QProcess::NormalExit && proc.exitStatus() != QProcess::NormalExit)
 		return false;
-	QString pkg_name = QDir::currentPath() + '\\' + Content_ID + "-A" + App_Ver.remove(2, 1) + "-V" + Version.remove(2, 1) + package_part_number + ".pkg";
-	if (!QDir().rename(QDir::currentPath() + '\\' + Content_ID + ".pkg", pkg_name))
+	App_Ver.remove(2, 1);
+	Version.remove(2, 1);
+	QString pkg_name = QDir::currentPath() + '\\' + Content_ID + "-A" + App_Ver + "-V" + Version + package_part_number + ".pkg";
+	if (!QDir().rename(QDir::currentPath() + '\\' + Content_ID + "-A" + App_Ver + "-V" + Version + ".pkg", pkg_name))
 		return false;
 	return true;
 }
