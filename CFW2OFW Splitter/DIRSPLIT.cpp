4f421a1e2c803dd71d7aef0fe7c1891f04ff1e87
Administrator <Administrator@AYASSIN-OUK981F>
2019-04-13 04:16:06 +0200

    Update project files.

--- CFW2OFW Splitter/DIRSPLIT.cpp
+++ CFW2OFW Splitter/DIRSPLIT.cpp
@@ -51,7 +51,7 @@ QStringList DIRSPLIT::split() {
 		if (!QFile(DestPath + f.filePath().mid(path.length())).exists())
 			return QStringList();
 	}
-	//if (!QDir(path).removeRecursively())
-		//return QStringList();
+	if (!QDir(path).removeRecursively())
+		return QStringList();
 	return splitteddirectorylist;
 }
