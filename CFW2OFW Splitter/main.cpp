79dbac1501271d53741cde9751634c48911f9a97
Administrator <Administrator@AYASSIN-OUK981F>
2019-04-13 03:42:58 +0200

    Update project files.

--- CFW2OFW Splitter/main.cpp
+++ CFW2OFW Splitter/main.cpp
@@ -27,33 +27,3 @@ int main(int argc, char *argv[])
 	return false;
 }
 
-//if (type == Han) {
-	//proc.start(ps3xploit_rifgen_edatresign, QStringList() << pkg_name);
-	//if (!proc.waitForStarted())
-		//return false;
-	//if (!proc.waitForFinished(-1))
-		//return false;
-	//if (proc.exitCode() != QProcess::NormalExit && proc.exitStatus() != QProcess::NormalExit)
-		//return false;
-	///if (!QDir().remove(pkg_name))
-		//return false;
-//}
-
-//
-//QStringList dirs(a.arguments());
-//dirs.removeFirst();
-//if (argc < 2)
-//	dirs << QDir(QDir::currentPath()).entryList(QStringList() << "BL??????" << "BC??????" << "NP??????", QDir::Dirs | QDir::NoDotAndDotDot);
-//if (dirs.isEmpty())
-//{
-//	qDebug() << "no valid directory BL?????? or BC?????? or NP?????? provided!" << endl << "Press any key to continue . . ."; getchar();
-//	return -1;
-//}
-//int i = 0;
-//for each (QString dir in dirs)
-//{
-//	if (dir.length() == 8)
-//		dir = QDir::currentPath() + QDir::separator() + dir;
-//	if (PKG(dir, PKG::Debug, 4294705152).generate_packages())
-//		i++;
-//}
