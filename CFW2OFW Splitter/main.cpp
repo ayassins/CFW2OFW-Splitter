799f8b23e6397f37b8e8850ef4dfc02d1722c207
a.yassin <a.yassin@msn.com>
2019-04-21 21:58:00 +0200

    Update project files.

--- CFW2OFW Splitter/main.cpp
+++ CFW2OFW Splitter/main.cpp
@@ -7,26 +7,30 @@
 #include "pkg.h"
 #include "dirsplit.h"
 
-int main(int argc, char *argv[])
-{
-	QCoreApplication a(argc, argv);
-	qDebug() << " --- CFW2OFW Toolbox v3 ---" << endl << "  -- a.yassin@msn.com --" << endl;
-	if (argc < 2)
-		qDebug() << "drag and drop directory BL?????? or BC?????? or NP??????.";
-	else {
-		QStringList gamespaths;
-		gamespaths << DIRSPLIT(argv[1], QStringList() << "PARAM.SFO" << "ICON0.PNG" << "USRDIR\\EBOOT.BIN", 4294705152).split();
-		if (!gamespaths.isEmpty())
-			for each (QString path in gamespaths)
-			{
-				qDebug() << path;
-				if (!PKG(path).generate_debug_package())
-					qDebug() << "couldn't generate game package!.";
-			}
-		else
-			qDebug() << "couldn't split game directory!.";
-	}
-	qDebug() << " Press any key to continue . . ."; getchar();
-	return false;
+
+void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg) {
+	if (type == QtDebugMsg)
+		fprintf(stdout, "%s\n", msg.toLocal8Bit().constData());
 }
+  int main(int argc, char *argv[]) {
+	  qInstallMessageHandler(myMessageOutput);
+	  QCoreApplication a(argc, argv);
+	  qDebug() << " --- CFW2OFW Toolbox v3 ---" << endl << "  -- a.yassin@msn.com --" << endl;
+	  if (argc < 2)
+		  qDebug() << "drag and drop directory BL?????? or BC?????? or NP??????.";
+	  else {
+		  QStringList gamespaths;
+		  gamespaths << DIRSPLIT(argv[1], QStringList() << "PARAM.SFO" << "ICON0.PNG" << "USRDIR\\EBOOT.BIN", 4294705152).split();
+		  if (!gamespaths.isEmpty())
+			  for each (QString path in gamespaths)
+			  {
+				  if (!PKG(path).generate_debug_package())
+					  qDebug() << "couldn't generate game package!.";
+			  }
+		  else
+			  qDebug() << "couldn't split game directory!.";
+	  }
+	  qDebug() << " Press any key to continue . . ."; getchar();
+	  return false;
+  }
 
