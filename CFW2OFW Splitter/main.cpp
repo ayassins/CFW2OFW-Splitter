74096c68d2f7e1f88502372d45ce2a88f38ba16a
a.yassin <a.yassin@msn.com>
2019-04-23 18:29:51 +0200

    Update project files.

--- CFW2OFW Splitter/main.cpp
+++ CFW2OFW Splitter/main.cpp
@@ -15,20 +15,18 @@ void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QS
   int main(int argc, char *argv[]) {
 	  qInstallMessageHandler(myMessageOutput);
 	  QCoreApplication a(argc, argv);
-	  qDebug() << " --- CFW2OFW Toolbox v3 ---" << endl << "  -- a.yassin@msn.com --" << endl;
+	  qDebug() << " --- CFW2OFW Splitter v1 ---" << endl << "  -- a.yassin@msn.com --" << endl;
 	  if (argc < 2)
-		  qDebug() << "drag and drop directory BL?????? or BC?????? or NP??????.";
+		  qDebug() << "drag and drop directory BL?????? or BC?????? or NP?????? or SL??????";
 	  else {
 		  QStringList gamespaths;
 		  gamespaths << DIRSPLIT(argv[1], QStringList() << "PARAM.SFO" << "ICON0.PNG" << "USRDIR\\EBOOT.BIN", 4294705152).split();
 		  if (!gamespaths.isEmpty())
 			  for each (QString path in gamespaths)
-			  {
 				  if (!PKG(path).generate_debug_package())
-					  qDebug() << "couldn't generate game package!.";
-			  }
-		  else
-			  qDebug() << "couldn't split game directory!.";
+					  qDebug() << "couldn't generate Package for [ " << path << " ]" << endl;
+				  else
+					  qDebug() << "couldn't split game directory!.";
 	  }
 	  qDebug() << " Press any key to continue . . ."; getchar();
 	  return false;
