992d1e1ed75103ac5776fe42444694cab2230127
a.yassin <a.yassin@msn.com>
2019-04-24 22:59:10 +0200

    Update project files.

--- CFW2OFW Splitter/main.cpp
+++ CFW2OFW Splitter/main.cpp
@@ -12,24 +12,28 @@ void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QS
 	if (type == QtDebugMsg)
 		fprintf(stdout, "%s", msg.toLocal8Bit().constData());
 }
-  int main(int argc, char *argv[]) {
-	  qInstallMessageHandler(myMessageOutput);
-	  QCoreApplication a(argc, argv);
-	  qDebug() << " --- CFW2OFW Splitter v1.1 ---" << endl << "  -- a.yassin@msn.com --" << endl;
-	  if (argc < 2)
-		  qDebug() << "drag and drop directory BL?????? or BC?????? or NP?????? or SL??????" << endl;
-	  else {
-		  QStringList gamespaths = DIRSPLIT(argv[1], QStringList() << "PARAM.SFO" << "ICON0.PNG" << "USRDIR\\EBOOT.BIN", 4294705152).split();
-		  if (!gamespaths.isEmpty()) {
-			  for each (QString path in gamespaths)
-				  if (!PKG(path).generate_debug_package())
-					  qDebug() << "couldn't generate Package for [ " << path << " ]" << endl;
-		  }
-		  else
-			  if (!PKG(argv[1]).generate_debug_package())
-				  qDebug() << "couldn't generate Package for [ " << argv[1] << " ]" << endl;
-	  }
-	  qDebug() << " Press any key to continue . . ."; getchar();
-	  return false;
-  }
+int main(int argc, char *argv[]) {
+	qInstallMessageHandler(myMessageOutput);
+	QCoreApplication a(argc, argv);
+	qDebug() << " --- CFW2OFW Splitter v1.1 ---" << endl << "  -- a.yassin@msn.com --" << endl;
+	QStringList argvdirs = a.arguments();
+	if (argvdirs.length() < 2)
+		qDebug() << "drag and drop directory BL?????? or BC?????? or NP?????? or SL??????" << endl;
+	else {
+		argvdirs.removeFirst();
+		for each (QString argvdir in argvdirs) {
+			QStringList gamespaths = DIRSPLIT(argvdir, QStringList() << "PARAM.SFO" << "ICON0.PNG" << "USRDIR\\EBOOT.BIN", 4294705152).split();
+			if (!gamespaths.isEmpty()) {
+				for each (QString path in gamespaths)
+					if (!PKG(path).generate_debug_package())
+						qDebug() << "couldn't generate Package for [ " << path << " ]" << endl;
+			}
+			else
+				if (!PKG(argvdir).generate_debug_package())
+					qDebug() << "couldn't generate Package for [ " << argvdir << " ]" << endl;
+		}
+	}
+	qDebug() << " Press any key to continue . . ."; getchar();
+	return false;
+}
 
