77093998e8d1798593b3428e6ecdd80baa6611cc
a.yassin <a.yassin@msn.com>
2019-04-25 21:50:44 +0200

    Update project files.

--- CFW2OFW Splitter/main.cpp
+++ CFW2OFW Splitter/main.cpp
@@ -4,6 +4,8 @@
 #include <Qtcore/QString>
 #include <Qtcore/qDebug>
 #include <QtCore/qstringlist.h>
+#include <QtCore/qcryptographichash.h>
+#include <QtCore/qstandardpaths.h>
 #include "pkg.h"
 #include "dirsplit.h"
 
@@ -12,6 +14,69 @@ void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QS
 	if (type == QtDebugMsg)
 		fprintf(stdout, "%s", msg.toLocal8Bit().constData());
 }
+
+
+QByteArray fileChecksum(const QString &fileName, QCryptographicHash::Algorithm hashAlgorithm) {
+	QFile f(fileName);
+	if (f.open(QFile::ReadOnly)) {
+		QCryptographicHash hash(hashAlgorithm);
+		if (hash.addData(&f)) {
+			return hash.result();
+		}
+	}
+	return QByteArray();
+}
+
+
+DWORD GetModuleSize(LPCSTR ModuleName) {
+	HMODULE hModule = GetModuleHandleA(ModuleName);
+	PIMAGE_DOS_HEADER IMAGE_DOS_HEADER = (PIMAGE_DOS_HEADER)hModule;
+	PIMAGE_NT_HEADERS IMAGE_NT_HEADERS = (PIMAGE_NT_HEADERS)((PBYTE)hModule + IMAGE_DOS_HEADER->e_lfanew);
+	PIMAGE_SECTION_HEADER IMAGE_SECTION_HEADER = (PIMAGE_SECTION_HEADER)((PBYTE)IMAGE_NT_HEADERS + sizeof(IMAGE_NT_HEADERS32));
+	DWORD maxpointer = 0, size = 0;
+	for (int i = 0; i < IMAGE_NT_HEADERS->FileHeader.NumberOfSections; ++i)
+	{
+		if (IMAGE_SECTION_HEADER->PointerToRawData > maxpointer)
+		{
+			maxpointer = IMAGE_SECTION_HEADER->PointerToRawData;
+			size = IMAGE_SECTION_HEADER->PointerToRawData + IMAGE_SECTION_HEADER->SizeOfRawData;
+		}
+		IMAGE_SECTION_HEADER++;
+	}
+	return size;
+}
+
+
+bool FileInstall(const QString &source, const QString &dest, bool overwrite = 0) {
+	QByteArray binary;
+	try {
+		QFile f(source);
+		if (!f.open(QFile::ReadOnly))
+			return false;
+		if (f.seek(GetModuleSize(source.toLocal8Bit().constData())))
+			return false;
+		binary = f.readAll();
+		f.close();
+	}
+	catch (...) {
+		return false;
+	}
+	try {
+		if ((QFile::exists(dest)) && (overwrite == false))
+			return true;
+		QFile f(dest);
+		if (!f.open(QFile::Truncate))
+			return false;
+		if (f.write(binary) != binary.length())
+			return false;
+		f.close();
+	}
+	catch (...) {
+		return false;
+	}
+	return true;
+}
+
 int main(int argc, char *argv[]) {
 	qInstallMessageHandler(myMessageOutput);
 	QCoreApplication a(argc, argv);
